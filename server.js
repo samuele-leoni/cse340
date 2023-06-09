4c2b0a63c06b2d91a73a6e88b00d4324f739c104
Samuele Leoni <leo22009@byui.edu>
2023-06-09 23:12:51 +0200

    Finished Implement index route with Express and EJS

--- server.js
+++ server.js
@@ -22,6 +22,10 @@ app.set("layout", "./layouts/layout") // not at views root
  * Routes
  *************************/
 app.use(static)
+// Index Route
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home" })
+})
 
 /* ***********************
  * Local Server Information
