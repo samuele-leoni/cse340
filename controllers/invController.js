6a59d7d2dc77cafb5c1efcd8c0706e979c5cb9d9
Samuele Leoni <leo22009@byui.edu>
2023-06-24 23:06:28 +0200

    Finished task 2

--- controllers/invController.js
+++ controllers/invController.js
@@ -7,32 +7,41 @@ const invCont = {}
  *  Build the inventory detail view
  * ************************** */
 invCont.buildInventoryDetail = async function (req, res, next) {
-    const inv_id = req.params.invId
-    const data = await invModel.getInventoryById(inv_id)
-    const vehicleData = data[0]
-    const vehicle = await utilities.buildInventoryDetails(vehicleData)
-    let nav = await utilities.getNav()
-    res.render("./inventory/detail", {
-        title: vehicleData.inv_year + " " + vehicleData.inv_make + " " + vehicleData.inv_model,
-        nav,
-        vehicle,
-    })
+    try {
+        const inv_id = req.params.invId
+        const data = await invModel.getInventoryById(inv_id)
+        const vehicleData = data[0]
+        const vehicle = await utilities.buildInventoryDetails(vehicleData)
+        let nav = await utilities.getNav()
+        res.render("./inventory/detail", {
+            title: vehicleData.inv_year + " " + vehicleData.inv_make + " " + vehicleData.inv_model,
+            nav,
+            vehicle,
+        })
+    } catch (error) {
+        utilities.handleErrors(error)
+    }
 }
 
 /* ***************************
  *  Build inventory by classification view
  * ************************** */
 invCont.buildByClassificationId = async function (req, res, next) {
-    const classification_id = req.params.classificationId
-    const data = await invModel.getInventoryByClassificationId(classification_id)
-    const grid = await utilities.buildClassificationGrid(data)
-    let nav = await utilities.getNav()
-    const className = data[0].classification_name
-    res.render("./inventory/classification", {
-        title: className + " vehicles",
-        nav,
-        grid,
-    })
+    try {
+        const classification_id = req.params.classificationId
+        const data = await invModel.getInventoryByClassificationId(classification_id)
+        const grid = await utilities.buildClassificationGrid(data)
+        let nav = await utilities.getNav()
+        const className = data[0].classification_name
+        res.render("./inventory/classification", {
+            title: className + " vehicles",
+            nav,
+            grid,
+        })
+    }
+    catch (error) {
+        utilities.handleErrors(error)
+    }
 }
 
 module.exports = invCont
