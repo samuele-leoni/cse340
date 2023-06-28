2cc429468ea94f866a380557a6b14ca86c05d6d9
Samuele Leoni <leo22009@byui.edu>
2023-06-28 22:09:11 +0200

    Finished step 3

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -7,4 +7,7 @@ const accountController = require("../controllers/accountController")
 // Route to build inventory by classification view
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
 
+// Route to build inventory by classification view
+router.get("/register", utilities.handleErrors(accountController.buildRegister));
+
 module.exports = router;
