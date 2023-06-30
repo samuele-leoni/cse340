ebfced93cf0fbd796bbfa0b10d8132a7b9991a88
Samuele Leoni <leo22009@byui.edu>
2023-06-30 11:52:45 +0200

    Step 4 done

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -13,10 +13,20 @@ router.get("/register", utilities.handleErrors(accountController.buildRegister))
 
 // Process the registration data
 router.post(
-    "/register",
-    regValidate.registationRules(),
-    regValidate.checkRegData,
-    utilities.handleErrors(accountController.registerAccount)
-  )
+  "/register",
+  regValidate.registationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount)
+)
+
+// Process the login attempt
+router.post(
+  "/login",
+  regValidate.loginRules(),
+  regValidate.checkLoginData,
+  (req, res) => {
+    res.status(200).send('login process')
+  }
+)
 
 module.exports = router;
