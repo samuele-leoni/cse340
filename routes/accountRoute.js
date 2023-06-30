9a4daf6e9d3cd3b20730fe7a5c06794524b8618b
Samuele Leoni <leo22009@byui.edu>
2023-06-30 10:52:34 +0200

    Finished data validation

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,6 +3,7 @@ const express = require("express")
 const router = new express.Router()
 const utilities = require("../utilities/")
 const accountController = require("../controllers/accountController")
+const regValidate = require('../utilities/account-validation')
 
 // Route to build the login view
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
@@ -10,7 +11,12 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin));
 // Route to build the register view
 router.get("/register", utilities.handleErrors(accountController.buildRegister));
 
-// Route for the register view
-router.post('/register', utilities.handleErrors(accountController.registerAccount))
+// Process the registration data
+router.post(
+    "/register",
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+  )
 
 module.exports = router;
